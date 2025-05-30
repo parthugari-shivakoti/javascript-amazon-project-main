d3d579484024f2eecb84cf4e0df96bff345853e7
Shiva koti <167282319+parthugari-shivakoti@users.noreply.github.com>
2025-05-30 10:03:30 +0530

    fixing radio-selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -50,7 +50,7 @@ cart.forEach((cartItem)=>{
 
                 <div class="delivery-option">
                   <input type="radio" class="delivery-option-input"
-                    name="delivery-option-2">
+                    name="delivery-option-${machingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -62,7 +62,7 @@ cart.forEach((cartItem)=>{
                 </div>
                 <div class="delivery-option">
                   <input type="radio" checked class="delivery-option-input"
-                    name="delivery-option-2">
+                    name="delivery-option-${machingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -74,7 +74,7 @@ cart.forEach((cartItem)=>{
                 </div>
                 <div class="delivery-option">
                   <input type="radio" class="delivery-option-input"
-                    name="delivery-option-2">
+                    name="delivery-option-${machingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
